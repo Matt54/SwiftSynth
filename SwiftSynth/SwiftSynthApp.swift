5548faa99aad02d356b33a4f0c70c6e39fce917c
Matt Pfeiffer <mattpfeiffer54@gmail.com>
2020-10-04 21:28:28 -0500

    Initial Commit

---
+++ SwiftSynth/SwiftSynthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSynthApp.swift
+//  SwiftSynth
+//
+//  Created by Matt Pfeiffer on 10/4/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSynthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
